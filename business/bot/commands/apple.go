cbf8bd15cdc29d8122d7627fc737fed09a008222
Maurizio Branca <maurizio.branca@gmail.com>
2025-01-02 18:18:50 +0100

    apple: improve not found messages

--- business/bot/commands/apple.go
+++ business/bot/commands/apple.go
@@ -26,11 +26,8 @@ func (c listAppleProducts) Regex() *regexp.Regexp {
 }
 
 func (c listAppleProducts) Execute(ctx context.Context, match []string) (string, error) {
-	var products []struct {
-		Name  string  `json:"name"`
-		URL   string  `json:"url"`
-		Price float64 `json:"price"`
-	}
+	var products []product
+	var filteredProducts []product
 
 	url := fmt.Sprintf("https://raw.githubusercontent.com/zmoog/refurbished-history/main/stores/%s/%s.json", match[1], match[2])
 	err := c.scraper.Scrape(url, &products)
@@ -38,15 +35,27 @@ func (c listAppleProducts) Execute(ctx context.Context, match []string) (string,
 		return "", err
 	}
 
-	// TODO: use Go templates to format the message instead of
-	// concatenating strings like an animal.
-	msg := "Products in stock:\n"
 	for _, p := range products {
 		if len(match) == 4 && !strings.Contains(p.Name, match[3]) {
 			continue
 		}
+		filteredProducts = append(filteredProducts, p)
+	}
+
+	if len(filteredProducts) == 0 {
+		return fmt.Sprintf("No %s in stock in %s", match[2], match[1]), nil
+	}
+
+	msg := "Products in stock:\n"
+	for _, p := range filteredProducts {
 		msg += fmt.Sprintf("- %s %v\n", p.Name, p.Price)
 	}
 
 	return msg, err
+}
+
+type product struct {
+	Name  string  `json:"name"`
+	URL   string  `json:"url"`
+	Price float64 `json:"price"`
 }
